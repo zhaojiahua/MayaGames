ae2093e31154316a472b225998ae04b50e32cc42
zhaojiahua <53359717+zhaojiahua@users.noreply.github.com>
2024-10-07 11:28:20 +0800

    Delete LangtonAnt directory

--- LangtonAnt/scripts/BtnCmdLines/UpspeedBtn_cmdlines.py
+++
@@ -1,4 +0,0 @@
-if Gl.moveSpeed<100:
-    Gl.moveSpeed+=1
-    cmds.intSlider('speedSlider',e=1,value=Gl.moveSpeed)
-    cmds.intSlider('speedSlider',e=1,bgc=Gl.SpeedSliderColorGradient.GetColor(Gl.moveSpeed/100.0))
